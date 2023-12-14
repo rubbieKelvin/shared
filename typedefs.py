79034582317d9efda9f1e513301157c4bca53c9f
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-12-14 16:02:34 +0100

    chore: fixed #11

--- typedefs.py
+++ typedefs.py
@@ -1,8 +1,5 @@
 import typing
 
-# roles essential to the backend functions
-InternalRole = typing.Literal["admin", "anonymous", "user"]
-
 Json: typing.TypeAlias = (
     int | str | bool | float | None | list["Json"] | dict[str, "Json"]
 )
