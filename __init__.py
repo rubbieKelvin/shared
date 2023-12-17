e716ea64c222247073c199dcad73cfd8797dc30c
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-12-17 01:33:05 +0100

    chore: added shortcuts

--- __init__.py
+++ __init__.py
@@ -2,3 +2,7 @@
 
 __version__ = "0.0.0"
 __author__ = {"name": "Rubbie kelvin", "email": "dev.rubbie@gmail.com"}
+
+# shortcuts
+from .view_tools.paths import Api
+from .view_tools.body_tools import validate, get_validated_body
