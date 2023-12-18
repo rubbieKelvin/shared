b64077fc05ff29cf0367330e40253da7a8771108
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-12-18 20:42:10 +0100

    chore: fixed pydantic issue in NotSet and changed little styles in authentication page

--- typedefs.py
+++ typedefs.py
@@ -1,4 +1,5 @@
 import typing
+from pydantic_core import core_schema, SchemaSerializer
 
 Json: typing.TypeAlias = (
     int | str | bool | float | None | list["Json"] | dict[str, "Json"]
@@ -22,3 +23,26 @@ class NotSet:
 
     def __repr__(self) -> str:
         return "NotSet"
+
+    @classmethod
+    def _validate(cls, value: "NotSet"):
+        return not value
+
+    @classmethod
+    def __get_pydantic_core_schema__(cls, source_type, handler):
+        _ = source_type
+        schema = core_schema.no_info_after_validator_function(
+            cls._validate,
+            handler(set),
+            serialization=core_schema.plain_serializer_function_ser_schema(
+                set,
+                info_arg=False,
+                return_schema=core_schema.set_schema(),
+            ),
+        )
+
+        # https://github.com/pydantic/pydantic/issues/7779#issuecomment-1775629521
+        cls.__pydantic_serializer__ = SchemaSerializer(
+            schema
+        )  # <-- this is necessary for pydantic-core to serialize
+        return schema
