21e7ed409eb78767fee9535777bf63971528390c
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-12-24 08:30:58 +0100

    chore: fixed a bug in shared that stopped auth apps from loading

--- __init__.py
+++ __init__.py
@@ -2,8 +2,3 @@
 
 __version__ = "0.0.0"
 __author__ = {"name": "Rubbie kelvin", "email": "dev.rubbie@gmail.com"}
-
-# shortcuts
-from .view_tools.paths import Api
-from .view_tools.pagination import paginate_by_queryparam
-from .view_tools.body_tools import validate, get_validated_body
---
+++ shortcut.py
@@ -0,0 +1,4 @@
+# shortcuts
+from .view_tools.paths import Api
+from .view_tools.pagination import paginate_by_queryparam
+from .view_tools.body_tools import validate, get_validated_body
